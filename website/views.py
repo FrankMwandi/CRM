b0f937882bdb9770d14b3ee8816100baee15693d
mwandi <frankmwandi93@gmail.com>
2023-07-08 17:57:56 +0300

    Completed login with an alert message

--- website/views.py
+++ website/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages 
 
 
 def home(request):
-    return render(request, 'home.html',{})
+    #checkout to see if logged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error, doublechek your credentials and try again")
+            return redirect('home')
+        
+    else:
+        return render(request, 'home.html',{})
 
-def login_user(request):
-    pass
+
 
 def logout_user(request):
     pass 
